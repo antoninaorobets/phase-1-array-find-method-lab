2a0875e3f3fc7e6edd6e81563a986289bddd13b5
antoninaorobets <pani.antonina.don@gmail.com>
2021-11-25 10:04:55 -0500

    commit tree 5175c55dea4abd18ba93cc17ba39c41d2cff4555

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"},
+    {year: "2015", result: "W"}
+  ]
 
+  function winYear(record){
+      return (record.result ==="W");
+  }
+
+
+function superbowlWin(record){
+
+    const winObject = (record.find(winYear))
+    console.log(winObject)
+    if (winObject === undefined) {
+      return  winObject;
+      }
+      else {
+       return winObject.year
+      };
+
+
+}
+
+
+
+
+
+ superbowlWin(record)
